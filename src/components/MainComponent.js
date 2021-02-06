502990b644b60279e52f3dfae7d25cac84221155
Matthew Stoner <mtstoner@me.com>
2021-02-05 22:51:20 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -36,6 +36,22 @@ class Main extends Component {
         />
       );
     };
+
+    const CampsiteWithID = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -46,6 +62,7 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/directory/:campsiteId" component={CampsiteWithID} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
