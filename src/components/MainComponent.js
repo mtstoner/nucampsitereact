21bd657d8ba59ae2c52e1e14ab7162278cf1506d
Matthew Stoner <mtstoner@me.com>
2021-02-06 11:45:38 -0500

    Added about component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -64,6 +65,11 @@ class Main extends Component {
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithID} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
